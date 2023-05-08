28e25def9018133e25bdfe8818864706e2d8ee0a
Genevieve Brown <gbrown@nhcweb.com>
2023-05-08 13:28:40 -0700

    Removed NHC specific files

--- future_model/01/Raven.bat
+++
@@ -1,8 +0,0 @@
-REM Run Raven for the first Run to get forecast and evap data - Generated from template file; overwritten for each Ensemble run
-
-cd C:\OkanaganGIS\Raven_ensembles\01\
-
-Raven Okanagan -o ./output/
-
-
-
